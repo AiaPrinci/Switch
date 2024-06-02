a322bee5caff9153230b1f91ebc5026c53367cfa
AiaPrinci <davidjimsheleishvili17@gmail.com>
2024-06-03 01:28:07 +0400

    eddited

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let container = document.querySelector('.container');
 let circle = document.querySelector('.circle');
 
+let body = document.querySelector('.body');
+
 container.addEventListener('click', () => {
     container.classList.toggle('night');
     circle.classList.toggle('moon');
+    body.classList.toggle('body-night');
 });
