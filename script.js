808d32bf0af62694b71d9506888247e74cc22ebf
AiaPrinci <davidjimsheleishvili17@gmail.com>
2024-06-03 01:46:27 +0400

    eddited

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let container = document.querySelector('.container');
 let circle = document.querySelector('.circle');
 
 let body = document.querySelector('.body');
+let innerNight = document.querySelector('.inner-container');
 
 container.addEventListener('click', () => {
     container.classList.toggle('night');
     circle.classList.toggle('moon');
     body.classList.toggle('body-night');
+    innerNight.classList.toggle('inner-container-night');
 });
